db64118bc7a0a32d24d140baac920c86fd39bf9f
Sujay-h-b <sujaygowda53@gmail.com>
2022-02-08 13:57:30 +0530

    IdDomain & app.properties recent update

--- src/main/java/com/suntech/domain/IdDomain.java
+++ src/main/java/com/suntech/domain/IdDomain.java
@@ -1,5 +1,7 @@
 package com.suntech.domain;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
@@ -7,6 +9,7 @@ import javax.persistence.MappedSuperclass;
 public abstract class IdDomain {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 
 	public Integer getId() {
