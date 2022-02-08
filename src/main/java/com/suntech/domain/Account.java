d5fee264f37309ce41c33d8339ca7dbdd71bf3b4
Saching5 <sachingr48@gmail.com>
2022-02-08 15:45:33 +0530

    Account and AccountType recent update

---
+++ src/main/java/com/suntech/domain/Account.java
@@ -0,0 +1,41 @@
+package com.suntech.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Account")
+public class Account {
+	
+
+	@Column(name="accountno")
+	private Long accountno;
+	@Column(name="balance")
+	private Double balance;
+	@Column(name="overdraft")
+	private Double overdraft;
+	
+	
+	public Long getAccountno() {
+		return accountno;
+	}
+	public void setAccountno(Long accountno) {
+		this.accountno = accountno;
+	}
+	public Double getBalance() {
+		return balance;
+	}
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+	public Double getOverdraft() {
+		return overdraft;
+	}
+	public void setOverdraft(Double overdraft) {
+		this.overdraft = overdraft;
+	}
+}
+	
+
+	
---
+++ src/main/java/com/suntech/domain/AccountType.java
@@ -0,0 +1,44 @@
+package com.suntech.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="AccountType")
+public class AccountType {
+	
+	@Column(name="transactionlimit")
+	private Double transactionlimit;
+	@Column(name="depositamt")
+	private Double depositamt;
+	@Column(name="withdrawllimit")
+	private Double withdrawllimit;
+	@Column(name="interestrate")
+	private Double interestrate;
+	public Double getTransactionlimit() {
+		return transactionlimit;
+	}
+	public void setTransactionlimit(Double transactionlimit) {
+		this.transactionlimit = transactionlimit;
+	}
+	public Double getDepositamt() {
+		return depositamt;
+	}
+	public void setDepositamt(Double depositamt) {
+		this.depositamt = depositamt;
+	}
+	public Double getWithdrawllimit() {
+		return withdrawllimit;
+	}
+	public void setWithdrawllimit(Double withdrawllimit) {
+		this.withdrawllimit = withdrawllimit;
+	}
+	public Double getInterestrate() {
+		return interestrate;
+	}
+	public void setInterestrate(Double interestrate) {
+		this.interestrate = interestrate;
+	}
+}
+	
