c4109e8375c5d4a54cc51de7cb0c6bb8b56530fc
Sujay-h-b <sujaygowda53@gmail.com>
2022-02-08 17:00:07 +0530

    ATM commit

---
+++ src/main/java/com/suntech/domain/Atm.java
@@ -0,0 +1,47 @@
+package com.suntech.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author sujay
+ *
+ */
+@Entity
+@Table(name = "atm")
+public class Atm extends IdDomain {
+
+	@Column(name = "location", nullable = false)
+	private String location;
+
+	@Column(name = "amountofcash", nullable = false)
+	private Double amountOfCash;
+
+	public Atm() {
+		super();
+	}
+
+	public Atm(String location, Double amountOfCash) {
+		super();
+		this.location = location;
+		this.amountOfCash = amountOfCash;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Double getAmountOfCash() {
+		return amountOfCash;
+	}
+
+	public void setAmountOfCash(Double amountOfCash) {
+		this.amountOfCash = amountOfCash;
+	}
+
+}
