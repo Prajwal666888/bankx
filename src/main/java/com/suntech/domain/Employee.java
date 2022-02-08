bc712f21a196db3f7f0dd39c807e109f30b5562b
PavanKumarSagar <pavanke77@gmail.com>
2022-02-08 15:23:50 +0530

    employee entity recent update

---
+++ src/main/java/com/suntech/domain/Employee.java
@@ -0,0 +1,55 @@
+package com.suntech.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="employee")
+public class Employee extends IdDomain  {
+
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="type")
+	private String type;
+	
+	@Column(name="salary")
+	private Double salary;
+
+	public Employee(String name, String type, Double salary) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+	
+}
