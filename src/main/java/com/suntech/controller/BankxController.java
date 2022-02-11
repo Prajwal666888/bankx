a32217f46bb12d106ea9c7bdff267686ba08dbf9
Saching5 <sachingr48@gmail.com>
2022-02-11 18:04:15 +0530

    rest api for the bank and branch

--- src/main/java/com/suntech/controller/BankxController.java
+++ src/main/java/com/suntech/controller/BankxController.java
@@ -5,12 +5,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.suntech.domain.Bank;
+import com.suntech.domain.Branches;
+import com.suntech.service.BankService;
+import com.suntech.service.BranchService;
+
+@RestController
 @Component
 public class BankxController {
 
 	@Autowired
 	private JmsTemplate jmsTemplate;
+	@Autowired
+	private BankService bankService;
+	@Autowired
+	private BranchService branchService;
 
 	@Value("${springjms.accountQueue}")
 	private String queue;
@@ -25,6 +38,18 @@ public class BankxController {
 	public void receiveFromAccountQueue(String message) {
 		System.out.println("Message Received===>" + message);
 
+	}
+
+	@PostMapping("/bank")
+	public Bank insertBank(@RequestBody() Bank bank) {
+		bankService.createAndSaveBank(bank);
+		return bank;
+	}
+
+	@PostMapping("/branch")
+	public Branches insertBranches(@RequestBody() Branches branches) {
+		branchService.createAndSaveBranch(branches);
+		return branches;
 	}
 
 }
--- src/main/java/com/suntech/service/support/BranchServiceImpl.java
+++ src/main/java/com/suntech/service/support/BranchServiceImpl.java
@@ -1,5 +1,22 @@
 package com.suntech.service.support;
 
-public class BranchServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.suntech.dao.BranchDao;
+import com.suntech.domain.Branches;
+import com.suntech.service.BranchService;
+
+@Service
+public class BranchServiceImpl implements BranchService {
+
+	@Autowired
+	private BranchDao branchDao;
+
+	@Override
+	public Branches createAndSaveBranch(Branches branches) {
+		branchDao.save(branches);
+		return branches;
+	}
 
 }
